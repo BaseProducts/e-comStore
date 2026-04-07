7dc6074fd23d2b55aab7a5cceec090ba0fa7a56a
BaseProducts <baseproductsus@gmail.com>
2026-04-07 23:07:13 +0530

    commented stripe

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -246,7 +246,7 @@ const Checkout = () => {
                             </div>
                             
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                                {["cod", "stripe"].map((method) => (
+                                {["cod"/*, "stripe"*/].map((method) => (
                                     <button
                                         key={method}
                                         type="button"
