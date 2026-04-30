4a95b0a7cbf42e385ea6cef45e602083a1c92b72
BaseProducts <baseproductsus@gmail.com>
2026-04-30 14:07:25 +0530

    removed motion

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -60,14 +60,14 @@ const HeroSection = () => {
         </motion.div>
       </div>
 
-      <motion.div
+      {/* <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1.5, duration: 1 }}
         className="absolute bottom-10 left-1/2 -translate-x-1/2"
       >
         <div className="w-[1px] h-16 bg-gradient-to-b from-transparent via-muted-foreground to-transparent animate-pulse" />
-      </motion.div>
+      </motion.div> */}
     </section>
   );
 };
